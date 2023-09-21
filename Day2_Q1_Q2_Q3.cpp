48ad96cb09e21fcdf90049fcf39d7c874e9743d2
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2023-09-21 22:12:40 +0530

    Day 2

---
+++ Day2_Q1_Q2_Q3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //   Q 1 : Declare integer variable and initilize with your age
+    
+    // int age = 21;
+    
+    //   Q 2 : create constant called PI and initilize 3.14159
+
+    // int PI = 3.14159;
+
+    
+    //   Q 3 : calculare area of circle ith r = 5
+
+    // int radius = 5;
+    // float area = 2*3.14*radius;
+    // cout<<"Area of circle :- "<<area;
+    return 0;
+}
