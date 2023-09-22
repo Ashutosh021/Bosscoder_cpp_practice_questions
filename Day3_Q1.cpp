69eeb15b3847447d63b985c232cb6d49ad7db417
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2023-09-22 22:19:57 +0530

    Day 3

---
+++ Day3_Q1.cpp
@@ -0,0 +1,16 @@
+// Q1  :  AP to take two number as input and print their output
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int num1,num2;
+    cout<<"\nEnter 1st number :- ";
+    cin>>num1;
+    cout<<"\nEnter 2nd number :- ";
+    cin>>num2;
+
+    int sum = num1 + num2;
+    cout<<"\nSum is :- "<<sum;
+    return 0;
+}
