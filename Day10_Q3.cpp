b93ce204ec4cb8886636ecd946e8d7adf7f1ce84
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2023-09-30 20:27:46 +0530

    Day 10

---
+++ Day10_Q3.cpp
@@ -0,0 +1,26 @@
+// advantage and disadvantage of array
+
+
+
+
+
+
+
+//    ***  Advantages of Arrays:  ***
+
+// Contiguous Memory Allocation
+// Random Access
+// Memory Efficiency 
+// Ease of Implementation 
+
+
+
+
+
+//    ***  Disadvantages of Arrays:  ***
+
+// Fixed Size
+// Wasted Memory 
+// Insertions and Deletions
+// Inflexible
+// Homogeneous Elements
