06603e27d0fdd9a802f058655e256b8ea4e45dc9
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2023-10-01 20:37:38 +0530

    Day 11

---
+++ Day11_Q2.cpp
@@ -0,0 +1,29 @@
+// Addition of 2D matrix
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr1[2][2] = {{1, 2}, {3, 4}};
+    int arr2[2][2] = {{3, 4}, {1, 2}};
+    int add[2][2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            add[i][j]=arr1[i][j]+arr2[i][j];
+        }
+        
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout<<add[i][j]<<" ";
+        }
+    }
+    return 0;
+}
