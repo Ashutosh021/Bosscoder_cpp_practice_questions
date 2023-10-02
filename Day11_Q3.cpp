aea7c75444764543efdacbabf1db7f989d542281
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2023-10-02 21:27:04 +0530

    Day 11

---
+++ Day11_Q3.cpp
@@ -0,0 +1,39 @@
+//3) Describe the use cases for multidimensional arrays and compare them to one-dimensional arrays
+
+
+
+
+// Multidimensional Arrays:
+
+//  1.   Matrix and Tabular Data:     to represent matrix-like structures, such as grids, tables, and spreadsheets.
+//                                    For example, a 2D array can represent a chessboard, a tic-tac-toe board.
+
+//  2.   Spatial Data:                such as 3D models .
+
+//  3.   Scientific Computing:       used to store and manipulate data like time-series data, seismic data.
+
+//  4.   Image Processing:           images are represented as 2D or 3D arrays of pixel values. You can apply various
+//                                    filters transformations, and operations to these arrays.
+
+//  5.  Matrices in Linear Algebra:      Matrix operations like addition, multiplication, and inversion are 
+//                                       performed using these arrays.
+
+
+
+
+
+// One-Dimensional Arrays:
+
+// 1. Sequential Data:      suitable for storing sequences of data, such as lists, 
+//                          queues, stacks, and dynamic arrays.
+
+// 2. Simple Collections:   for implementing collections like lists, sets, and bags when no specific ordering 
+//                          or structure is needed.
+
+// 3. Efficient Storage:    1D arrays are more memory-efficient and straightforward to work with compared 
+//                          to multidimensional arrays.
+
+// 4. Linear Search:        If you primarily perform linear searches or iteration through a list of items,
+//                           a one-dimensional array is a straightforward choice.
+
+
