7bfdd2dd0861d3716cc3f0912af25fbb8af810c8
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2023-10-03 20:53:26 +0530

    Day 12

---
+++ Day12_Q1.cpp
@@ -0,0 +1,22 @@
+//  reverse a string 
+
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    string str;
+    cout<<"Enter a string :- ";
+    getline(cin,str);
+
+    int i = 0;
+    int j = str.length()-1;
+    while(i<=j){
+        swap(str[i],str[j]);
+        i++;
+        j--;
+    }
+
+    cout<<str;
+    return 0;
+}
