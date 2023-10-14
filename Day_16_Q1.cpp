133954038ea10d1e5b1bdbba714ee8b0d9623d63
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2023-10-14 20:56:10 +0530

    Day 16

---
+++ Day_16_Q1.cpp
@@ -0,0 +1,21 @@
+// Add constructors to your car class and create car objects&
+#include<iostream>
+using namespace std;
+
+class car{
+    private:
+        string company;
+        string model;
+        string color;
+        int price;
+        string oilType;
+
+    car(){
+
+    }
+};
+
+int main(){
+    car c1;
+    return 0;
+}
