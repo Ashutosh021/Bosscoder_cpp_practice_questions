1e52fc0b7d1fb2b79f4ca285d8a0da4847627b9b
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2023-12-04 21:39:19 +0530

    Day 22

---
+++ Day_22_Q4.cpp
@@ -0,0 +1,62 @@
+// // Explain the concept of inheritance
+// // and its advantages in object-oriented
+// // programming.
+
+
+// **Concept of Inheritance in Object-Oriented Programming (OOP):**
+
+// Inheritance is a fundamental concept in object-oriented programming (OOP) that allows a new class, called a derived class or subclass, to inherit attributes and behaviors from an existing class, known as the base class or superclass. The derived class can then extend or override these inherited properties, as well as add new attributes and behaviors specific to itself.
+
+
+// There are various types of inheritance relationships:
+
+// 1. **Single Inheritance:** 
+// A class inherits from only one base class.
+
+// 2. **Multiple Inheritance:** 
+// A class inherits from more than one base class.
+
+// 3. **Multilevel Inheritance:** 
+// A class inherits from another class, and then another class inherits from it, forming a chain of inheritance.
+
+// 4. **Hierarchical Inheritance:** 
+// Multiple classes inherit from a single base class.
+
+// 5. **Hybrid Inheritance:**
+// This is the combinationof two or more than two inheritance.
+
+
+
+// **Advantages of Inheritance in OOP:**
+
+
+// 1. **Code Reusability:**
+//    - Inheritance promotes code reuse by allowing a new class to use the properties and behaviors of an existing class. The derived class automatically has access to the members of the base class.
+
+
+// 2. **Polymorphism:**
+//    - Inheritance supports polymorphism, where objects of the derived class can be treated as objects of the base class. This enables more flexible and dynamic code, as different classes can be used interchangeably.
+
+
+// 3. **Logical Organization:**
+//    - Inheritance facilitates a hierarchical and logical organization of code, modeling relationships between classes based on their similarities and differences. This makes the code more intuitive and easier to understand.
+
+
+// 4. **Extensibility:**
+//    - New functionality can be added to a program by creating a new class that inherits from an existing class. This extends the capabilities of the software without modifying the existing code.
+
+
+// 5. **Maintenance:**
+//    - Changes made to the base class automatically affect all the derived classes. This simplifies maintenance and updates, as modifications are localized to the base class.
+
+
+// 6. **Abstraction:**
+//    - Inheritance allows you to create abstract classes that define a common interface for a group of related classes. This abstraction helps in designing systems with clear separation of concerns.
+
+
+// 7. **Flexibility:**
+//    - Inheritance provides flexibility by allowing the creation of new classes that retain the characteristics of existing classes. This adaptability is particularly useful for accommodating changes in requirements.
+
+
+// 8. **Reduction of Code Duplication:**
+//    - Common functionalities and attributes are defined in the base class, reducing the need for redundant code in the derived classes. This not only saves development time but also minimizes the chance of errors.
